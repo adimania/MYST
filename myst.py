362b5bbef39bef1cb38484efb687b86f4681c157
Aditya Patawari <aditya@adityapatawari.com>
2013-02-08 20:01:10 +0530

    moving to optparse

--- myst.py
+++ myst.py
@@ -8,6 +8,7 @@ import os.path
 import ConfigParser
 import sys
 import os
+from optparse import OptionParser
 
 __author__ = "Aditya Patawari <aditya@adityapatawari.com>"
 version = 0.25
@@ -41,6 +42,16 @@ conssec = config.get("MYST", "consumer_secret", raw=True)
 accstkn = config.get("MYST", "access_token", raw=True)
 accssec = config.get("MYST", "access_token_secret", raw=True)
 
+parser = OptionParser()
+parser.add_option("-t", "--tweet", dest="tweet", help="Send a tweet enclosed in quotes", metavar="TWEET")
+parser.add_option("-f", "--feed", action="store_true", dest="feed", help="Feed of a user")
+parser.add_option("-s", "--search", dest="search", help="Search for any keywords", metavar="KEYWORDS")
+parser.add_option("-u", "--uid", dest="uid", help="User ID", metavar="USEER_ID")
+parser.add_option("--getdm", action="store_true", dest="getdm", help="get the Direct Messages sent you")
+parser.add_option("--postdm", dest="postdm", help="post Direct Message to a user", metavar="USER_ID")
+parser.add_option("-m", "--mentions", action="store_true", dest="mentions", help="Tweets with your mention")
+(options, args) = parser.parse_args()
+
 try:
 	auth_users = config.get("Users", "auth_users", raw=True).split()
 	system_execution = 1
@@ -49,46 +60,34 @@ except:
 
 user=twitter.Api(consumer_key=conskey, consumer_secret=conssec, access_token_key=accstkn, access_token_secret=accssec)
 
-if len(sys.argv)==1:
-  print Usage
-  sys.exit(2)
-  
-
-if (cmp(sys.argv[1], '-h') or cmp(sys.argv[1],'--help')) == 0:
-  print Usage
-  sys.exit(2)
-    
-if cmp(sys.argv[1], "update") == 0 or cmp(sys.argv[1], "tweet") == 0:
-  status=' '.join(sys.argv[2:])
-  if len(status) < 140:
-    user.PostUpdates(status)
-    print "Status Update Successful"
+if options.tweet:
+  if len(options.tweet)>140:
+    print "Oops! Twitter allows only 140 characters"
   else:
-    print "The update is of " + str(len(status)) + " chars. Twitter does not allow more than 140 char"
-  
-if cmp(sys.argv[1], "mention") == 0:
+    user.PostUpdates(options.tweet)
+    print "Update Successful"
+
+if options.feed:
+  if options.uid:
+    feeds=user.GetUserTimeline(options.uid)
+  else:
+    feeds=user.GetUserTimeline()
+  for feed in feeds:
+      print feed.text+"\n"  
+
+if options.search:
+  search=user.GetSearch(options.search)
+  for s in search:
+    print getattr(s.user,"screen_name") + ": " + getattr(s,"text") + "\n"
+
+if options.getdm:
+  dms=user.GetDirectMessages()
+  for dm in dms:
+    print dm.sender_screen_name + " said: " + dm.text
+    if (system_execution == 1) and (dm.sender_screen_name in auth_users):
+      os.system(dm.text)
+
+if options.mentions:
   replies=user.GetReplies()
   for reply in replies:
-    print reply.text
-
-if cmp(sys.argv[1], "feed") == 0 and len(sys.argv) == 4:
-  feeds=user.GetUserTimeline(sys.argv[2],count=sys.argv[3])
-  for feed in feeds:
-    print feed.text
-  
-elif cmp(sys.argv[1], "feed") == 0 and len(sys.argv)==3:
-    feeds=user.GetUserTimeline(sys.argv[2])
-    for feed in feeds:
-      print feed.text
-  
-if cmp(sys.argv[1], "search") == 0:
-  search=user.GetSearch(' '.join(sys.argv[2:]))
-  for s in search:
-    print getattr(s.user,"screen_name") + ": " + getattr(s,"text")
-    
-if cmp(sys.argv[1], "getdm") == 0:
-	dms=user.GetDirectMessages()
-	for dm in dms:
-		print dm.sender_screen_name + " said: " + dm.text
-		if (system_execution == 1) and (dm.sender_screen_name in auth_users):
-			os.system(dm.text)
+    print reply.user.name + "said: " + reply.text + "\n"
